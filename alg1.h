faaba0f9415f1462cf9a535b4703214deda1bda5
bssipes <bssipes@gmail.com>
2014-04-19 15:59:44 -0500

    Update alg1.h
    Makes it to the first "forced" message, then errros

--- alg1.h
+++ alg1.h
@@ -27,36 +27,39 @@ void alg1(string minmax, vector<vector<int> > fData) //Greedy - Immediate Neighb
 		{
 			cout << endl << "Current Reward: " << Reward << endl;
 			cout << j << "," << i << ": ";
-			if (i+1<= w) ////////////////////
+			if (i+1<= w)
 			{
-				if (fData[i+1][j]<fData[i][j+1])
+				if (j+1<=h)
 				{
-					cout << fData[i+1][j] << " < " << fData[i][j+1] << " thus I CHOOSE to go RIGHT.\n ";
-					Path.append("R");
-					i++;
-					Reward += fData[i][j]; //i was increased before the summation, no need to look ahead at i+1
-					PathReward += fData[i][j] + "+";
+					if (fData[i+1][j]<fData[i][j+1])
+					{
+						cout << fData[i+1][j] << " < " << fData[i][j+1] << " thus I CHOOSE to go RIGHT.\n ";
+						Path.append("R");
+						i++;
+						Reward += fData[i][j]; //i was increased before the summation, no need to look ahead at i+1
+						PathReward += fData[i][j] + "+";
+					}
+					else
+					{
+						cout << fData[i+1][j] << " > " << fData[i][j+1] << " thus I CHOOSE to go UP.\n";
+						Path.append("U");
+						j++;
+						Reward += fData[i][j]; //j was increased before the summation, no need to look ahead at j+1
+						PathReward += fData[i][j] + "+";
+					}
 				}
-				else if (j+1<=w) /////////////////
+				else //j+1>h
 				{
-					cout << fData[i+1][j] << " > " << fData[i][j+1] << " thus I CHOOSE to go UP.\n";
-					Path.append("U");
-					j++;
-					Reward += fData[i][j]; //j was increased before the summation, no need to look ahead at j+1
-					PathReward += fData[i][j] + "+";
-				}
-				else
-				{
-					cout << "I cannot increase j any more. j = " << j << "so I'm FORCED to go RIGHT.\n";
+					cout << "I cannot increase j any more. j = " << j << " so I'm FORCED to go RIGHT (+"<<fData[i+1][j]<<")\n";
 					Path.append("R");
 					i++;
 					Reward += fData[i][j]; //i was increased before the summation, no need to look ahead at i+1
 					PathReward += fData[i][j] + "+";	
 				}
 			}
-			else //if increasing i would go off the grid
+			else //i+1>w
 			{
-				cout << "I cannot increase i any more. i = " << i << " so I'm FORCED to go UP.\n";
+				cout << "I cannot increase i any more. i = " << i << " so I'm FORCED to go UP (+"<<fData[i][j+1]<<")\n";
 				if (j+1<=w)
 				{
 					Path.append("U");
@@ -71,56 +74,56 @@ void alg1(string minmax, vector<vector<int> > fData) //Greedy - Immediate Neighb
 			}
 		}
 	}
-	else //if minmax =/= "min" then I assume it means max. I don't want to reprompt the user for a valid input.
-	{
-		cout << "Alg1 MAX\n";
-		while ((i !=h) && (j !=w))
-		{
-			cout << j << "," << i << ": ";
-			if (i+1<= h)
-			{
-				if (fData[i+1][j]>fData[i][j+1])
-				{
-					cout << fData[i+1][j] << " > " << fData[i][j+1] << " thus I CHOOSE to go UP.\n";
-					Path.append("U");
-					i++;
-					Reward += fData[i][j]; //i was increased before the summation, no need to look ahead at i+1
-					PathReward += fData[i][j] + "+";	
-				}
-				else if (j+1<=w)
-				{
-					cout << fData[i+1][j] << " < " << fData[i][j+1] << " thus I CHOOSE to go RIGHT.\n";
-					Path.append("R");
-					j++;
-					Reward += fData[i][j]; //j was increased before the summation, no need to look ahead at j+1
-					PathReward += fData[i][j] + "+";
-				}
-				else
-				{
-					cout << "I cannot increase j any more. j = " << j << "so I'm FORCED to go UP.\n";
-					Path.append("U");
-					i++;
-					Reward += fData[i][j]; //i was increased before the summation, no need to look ahead at i+1
-					PathReward += fData[i][j] + "+";	
-				}
-			}
-			else //if increasing i would go off the grid
-			{
-				cout << "I cannot increase i any more. i = " << i << " so I'm FORCED to go RIGHT.\n";
-				if (j+1<=w)
-				{
-					Path.append("R");
-					j++;
-					Reward += fData[i][j];
-					PathReward += fData[i][j] + "+";
-				}
-				else //this should never be reached because i=h and j=w should break out of the while loop.
-				{
-					cout << "Hit error statement 2\ni,j = " << i << "," << j << "\nh,w = " << h << "," << w;
-				}
-			}
-		}
-	}
+	//else //if minmax =/= "min" then I assume it means max. I don't want to reprompt the user for a valid input.
+	//{
+	//	cout << "Alg1 MAX\n";
+	//	while ((i !=h) && (j !=w))
+	//	{
+	//		cout << j << "," << i << ": ";
+	//		if (i+1<= h) //(NEED TO CHECK BOTH BOUNDS, DUMMY)
+	//		{
+	//			if (fData[i+1][j]>fData[i][j+1])
+	//			{
+	//				cout << fData[i+1][j] << " > " << fData[i][j+1] << " thus I CHOOSE to go UP.\n";
+	//				Path.append("U");
+	//				i++;
+	//				Reward += fData[i][j]; //i was increased before the summation, no need to look ahead at i+1
+	//				PathReward += fData[i][j] + "+";	
+	//			}
+	//			else if (j+1<=w)
+	//			{
+	//				cout << fData[i+1][j] << " < " << fData[i][j+1] << " thus I CHOOSE to go RIGHT.\n";
+	//				Path.append("R");
+	//				j++;
+	//				Reward += fData[i][j]; //j was increased before the summation, no need to look ahead at j+1
+	//				PathReward += fData[i][j] + "+";
+	//			}
+	//			else
+	//			{
+	//				cout << "I cannot increase j any more. j = " << j << "so I'm FORCED to go UP.\n";
+	//				Path.append("U");
+	//				i++;
+	//				Reward += fData[i][j]; //i was increased before the summation, no need to look ahead at i+1
+	//				PathReward += fData[i][j] + "+";	
+	//			}
+	//		}
+	//		else //if increasing i would go off the grid
+	//		{
+	//			cout << "I cannot increase i any more. i = " << i << " so I'm FORCED to go RIGHT.\n";
+	//			if (j+1<=w)
+	//			{
+	//				Path.append("R");
+	//				j++;
+	//				Reward += fData[i][j];
+	//				PathReward += fData[i][j] + "+";
+	//			}
+	//			else //this should never be reached because i=h and j=w should break out of the while loop.
+	//			{
+	//				cout << "Hit error statement 2\ni,j = " << i << "," << j << "\nh,w = " << h << "," << w;
+	//			}
+	//		}
+	//	}
+	//}
 	PathReward.erase(PathReward.length()-1); //remove the trailing + before printing
 	cout << "End reached! Reward: " + Reward << "\nPath taken: " << Path;
 	return;
