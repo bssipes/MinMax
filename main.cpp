a1c80158fb6c351b2cfc88fba5c79c81eb4777d2
bssipes <bssipes@gmail.com>
2014-04-19 11:30:29 -0500

    Copied in v0.2 of main.cpp
    1>main.obj : error LNK2005: "void __cdecl alg1(class std::basic_string<char,struct std::char_traits<char>,class std::allocator<char> >,class std::vector<class std::vector<int,class std::allocator<int> >,class std::allocator<class std::vector<int,class std::allocator<int> > > >)" (?alg1@@YAXV?$basic_string@DU?$char_traits@D@std@@V?$allocator@D@2@@std@@V?$vector@V?$vector@HV?$allocator@H@std@@@std@@V?$allocator@V?$vector@HV?$allocator@H@std@@@std@@@2@@2@@Z) already defined in agl1.obj
    1>C:\Users\Ben\Documents\Visual Studio 2010\Projects\CS404MinAndMax\Debug\CS404MinAndMax.exe : fatal error LNK1169: one or more multiply defined symbols found

---
+++ main.cpp
@@ -0,0 +1,87 @@
+/* Written by Ben Sipes, except where specifically sourced
+CS404 Algorithms
+April 22, 2014
+Version 0.2
+CHANELOG:
+0.1 File created, no functionality
+0.2 Able to parse the provided example file
+
+PRE-ALGORITHM SETUP
+1) Read in a source file of numbers and GENERATE an 'array' based on this file
+(Potentially CSV or seperated by spaces / new lines. Data structure is not set in stone and should be tested for efficiency)
+2) Add in code to time the algorithm
+(SVC-calls, CPTime)
+
+
+Algorithm 1: Greedy-Immediate Neighbors
+Compare all 1-step options (i+1, j+1) (up or right), choose LARGEST/SMALLEST (WARNING: STAY IN BOUNDS!), ADD to sum, RECORD path
+
+
+Algorithm 2: Greedy- Min/Max Values of Array
+Make a copy of the array and sort it by value
+Go directly to the largest value 
+(option 1: all right until correct column, then up to correct row)
+(option 2: all up until correct row, then right to correct column)
+Go to next largest (target[i] >= current[i] and target[j] >= current[j]) by the same option
+
+Algorithm 3: Random Choice
+While this makes no attempt to be the best, it'll help to make sure I have safeguards in for going out of bounds
+Test twice, pretend largest is the "approximate" maximum and the smaller is the "approximate" minimum.
+
+Algorithm 4: 
+Sum all choices by row and by column
+For each choice, choose whichever has a smaller sum (to maximize)
+or larger sum (minimize)
+If they're equal, greedy (choose whichever is higher/lower of the immediate choices)
+(NOTE: I will need to constantly update the values for each row and column as I move. Is this worth it?)
+
+
+Version History: 
+0.1: Only comments. Wrote up general algorithms for 1, 2, and 3 and described the problem setup steps
+*/
+
+#include <fstream>
+#include <array>
+#include <list>
+#include "agl1.h"
+//#include <string>
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+
+vector<vector<int> > readFile(string filename)
+{
+	vector<vector<int>> fileData;
+	vector<int> currentRow;
+	ifstream fin(filename);
+	string line;
+	std::getline(fin,line, ','); //read in the first line until a comma, which is the height
+	int height = atoi(line.c_str());
+	std::getline(fin,line); //the rest of the first line is the width
+	int width = atoi(line.c_str());
+	
+	for(unsigned k=0;k<height;k++)
+	{
+		for(unsigned l=0;l<width;l++)
+		{
+			std::getline(fin,line, ','); //read in x value, discard
+				std::getline(fin,line, ','); //read in y value, discard
+			std::getline(fin,line); //read in the actual value
+			currentRow.push_back(atoi(line.c_str())); //converts the value from str to int
+//			cout << l;
+		}
+		fileData.push_back(currentRow); //places currentRow instead fileData vector
+//		cout << "Finished reading row " << k << endl;
+	}
+	fin.close();
+//	cout << "Finished reading the whole file." << endl;
+	return fileData;
+}
+
+void main()
+{
+	vector<vector<int> > fileData = readFile("data.txt");
+	//alg1("min",fileData);
+	//alg1("max",fileData);
+	return;
+}
