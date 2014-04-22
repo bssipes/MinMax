83fbf49d1270cbd28987b70e96f8ac6ab19ec746
bssipes <bssipes@gmail.com>
2014-04-22 01:07:37 -0500

    Update choose.h
    Added support for (bool, float, float)

--- choose.h
+++ choose.h
@@ -30,3 +30,31 @@ char choose(bool min, int U, int R)
 	}
 	return result;
 }
+
+char choose(bool min, float U, float R)
+{
+	char result;
+	if (min == true)
+	{
+		if (U < R)
+		{
+			result = 'U';
+		}
+		else // if R is less than or equal, choose R
+		{
+			result = 'R';
+		}
+	}
+	else if (min == false)
+	{
+		if (U > R)
+		{
+			result = 'U';
+		}
+		else //if R is larger or equal, choose R
+		{
+			result = 'R';
+		}
+	}
+	return result;
+}
